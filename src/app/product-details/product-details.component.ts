03adfad46efda99df43c01c97bec2d026c898be1
AlessiRizzo <99805854+AlessiRizzo@users.noreply.github.com>
2023-02-16 14:36:14 +0100

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,17 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {}
-  //iniettare ActivateRoute nel costruttore
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+  //iniettare ActivateRoute nel costruttore e iniettare il servizio carrello
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+  //prende product come argomento, usa il metodo per aggiungere il prodotto al carrello e visualizza il messaggio
 
   ngOnInit() {
     //prendi l'id del prodotto dalla route
