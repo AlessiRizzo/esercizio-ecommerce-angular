d7156edcb0c3eca8ea93e469ca4bdc32a7d145c7
AlessiRizzo <99805854+AlessiRizzo@users.noreply.github.com>
2023-02-16 15:09:10 +0100

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 
@@ -5,20 +6,31 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CartService {
+  constructor(private http: HttpClient) {}
   items: Product[] = [];
   //proprietà per memorizzare l'array dei prodotti correnti nel carrello
+  //
   addToCart(product: Product) {
     this.items.push(product);
   }
   //aggiunge un prodotto a array items
+  //
   getItems() {
     return this.items;
   }
   //raccoglie cosa gli utenti mettono nel carrello e restituisce ogni articolo con la quantità associata
+  //
   clearCart() {
     this.items = [];
     return this.items;
   }
   //restituisce un array vuoto per svuotare il carrello
-  constructor() {}
+  //
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
+  //metodo che usa HttpClient con il metodo get per ricevere dati API
+  //
 }
