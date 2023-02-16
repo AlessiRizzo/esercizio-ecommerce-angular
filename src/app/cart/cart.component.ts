96e1b18584b63a39e88a0349a809a56d66da5520
AlessiRizzo <99805854+AlessiRizzo@users.noreply.github.com>
2023-02-16 15:54:01 +0100

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -9,5 +10,21 @@ import { CartService } from '../cart.service';
 export class CartComponent {
   //proprietà per memorizzare i prodotti nel carrello
   items = this.cartService.getItems();
-  constructor(private cartService: CartService) {}
+
+  //prende nome e indirizzo
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  //metodo per elaborare il modulo, il metodo permette agli utenti di inviare i dati
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
