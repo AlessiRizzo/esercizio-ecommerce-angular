6f90442eb1877ddef54eacf023248f51f3ef59fc
AlessiRizzo <99805854+AlessiRizzo@users.noreply.github.com>
2023-02-16 12:41:21 +0100

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent {
+  product: Product | undefined;
 
+  constructor(private route: ActivatedRoute) {}
+  //iniettare ActivateRoute nel costruttore
+
+  ngOnInit() {
+    //prendi l'id del prodotto dalla route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    //trova il prodotto che corrisponde all'id trovato in route
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
 }
